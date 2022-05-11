bf747f5b68153532cba8d6b08a608c241f72f399
thomasseo1 <t.seo8456@gmail.com>
2022-05-11 13:57:04 -0700

    added the useEffect and some hooks

--- client/containers/Router.jsx
+++ client/containers/Router.jsx
@@ -6,7 +6,6 @@ import CocktailContainer from "./CocktailContainer.jsx";
 import UserBarContainer from "./UserBarContainer.jsx";
 import SignIn from "../components/Login.jsx";
 
-
 /**
  * Renders the navbar which has links to all Routes below.
  * Otherwise, routes and navigates users to the three main pages and their stateful containers
@@ -16,8 +15,9 @@ export default function Router() {
     <div className="router">
       <BrowserRouter>
         <Navbar />
-        <Routes> 
-          <Route path="/signIn" element={<SignIn />}/>
+        <Routes>
+          <Route path="/" element={<SignIn />} />
+          <Route path="/signup" element={<SignUp />} />
           <Route path="/listPlaceholder" element={<ShoppingListContainer />} />
           <Route path="/cocktails" element={<CocktailContainer />} />
           <Route path="/userbar" element={<UserBarContainer />} />
